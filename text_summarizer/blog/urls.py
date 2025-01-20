c18d846d2104a00207422af677bd83dfcac47805
tanishqtqs <55868345+tanishqtqs@users.noreply.github.com>
2025-01-20 22:11:47 +0530

    initial setup

---
+++ text_summarizer/blog/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('create/', views.create_blog, name='create_blog'),
+    path('list/', views.list_blogs, name='list_blogs'),
+]
