a099eea139e729d4a6c01bcb122827c98572c27a
tanishqtqs <55868345+tanishqtqs@users.noreply.github.com>
2025-01-22 23:58:02 +0530

    comment urls

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('create/', views.create_blog, name='create_blog'),
-    path('list/', views.list_blogs, name='list_blogs'),
+    # path('create/', views.create_blog, name='create_blog'),
+    # path('list/', views.list_blogs, name='list_blogs'),
     path('ping/', views.ping, name='ping'),
 ]
