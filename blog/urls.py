b179c05e2408fc7d00005ee6573945c6992772bc
tanishqtqs <55868345+tanishqtqs@users.noreply.github.com>
2025-01-22 23:56:40 +0530

    Add ping API to test server

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('create/', views.create_blog, name='create_blog'),
     path('list/', views.list_blogs, name='list_blogs'),
+    path('ping/', views.ping, name='ping'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
 # Create your views here.
+from django.http import JsonResponse
+
+def ping(request):
+    return JsonResponse({'message': 'Server is running!'})
